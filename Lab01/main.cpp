75ab87b3aea0472dcfa3c7b71f0fc165b12bc441
SymplyAHuman <43094600+EvgeniiMendeleev@users.noreply.github.com>
2019-09-12 22:13:48 +0300

    Update main.cpp

--- Lab01/main.cpp
+++ Lab01/main.cpp
@@ -1,6 +1,3 @@
-// Laboratory work №1.cpp: определяет точку входа для консольного приложения.
-//
-
 #include "stdafx.h"
 #include <iostream>
 #include <fstream>
@@ -13,13 +10,16 @@ using namespace std;
 
 int main()
 {
+	/*!!!ДАННЫЙ ФАЙЛ ЯВЛЯЕТСЯ ТЕСТОВЫМ!!!
+	!!!ЗДЕСЬ ПРОВЕРЯЕТСЯ РАБОТОСПОСОБНОСТЬ ВСЕХ МЕТОДОВ КАЖДОГО КЛАССА!!!*/
+
 	setlocale(LC_ALL, "Russian");
 
-	Student FirstStudent("Иванов", "Иван", "Иванович");
-	Student Second("Менделеев", "Евгений", "Андреевич");
-	Student Thrid("Монахов", "Иван", "Михайлович");
-	Student Four("Андриенко", "Степан", "Константинович");
-	Student Five("Алексеева", "Валерия", "Александровна");
+	Student FirstStudent("Иванов", "Иван", "Иванович", Perfectly);
+	Student Second("Менделеев", "Евгений", "Андреевич", Good);
+	Student Thrid("Монахов", "Иван", "Михайлович", Bad);
+	Student Four("Андриенко", "Степан", "Константинович", Perfectly);
+	Student Five("Алексеева", "Валерия", "Александровна", Perfectly);
 
 	StudentGroup group7091(7091);
 	StudentGroup group8032(8032);
@@ -31,14 +31,14 @@ int main()
 	group7091.set_student(&Five);
 	group8032.set_student(&Thrid);
 
-	vector<Student*> students = group7091.get_sort_students(lexicographic);
+	//vector<Student*> students = group7091.get_sort_students(marks);
 
-	for (unsigned i = 0; i < students.size(); i++)
-	{
-		students[i]->show_student();
-		cout << endl;
-		cout << endl;
-	}
+	//for (unsigned i = 0; i < students.size(); i++)
+	//{
+	//	students[i]->show_student();
+	//	cout << endl;
+	//	cout << endl;
+	//}
 
 	/*group7091.show_students();
 	cout << endl;
