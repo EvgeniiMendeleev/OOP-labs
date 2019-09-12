78810dba8284f84d509bc7d76a5268b2fd69c0a9
SymplyAHuman <43094600+EvgeniiMendeleev@users.noreply.github.com>
2019-09-12 22:08:10 +0300

    Update Student.h

--- Lab01/Student.h
+++ Lab01/Student.h
@@ -8,19 +8,22 @@ using namespace std;
 
 class StudentGroup;
 
+enum TypeOfPerformance {Perfectly, Good, Bad, Nothing};
+
 class Student
 {
 	string firstName;					//Имя
 	string lastName;					//Фамилия
 	string patronymic;					//Отчество
 
+	TypeOfPerformance performance;
 	vector<unsigned> numbersOfGroup;	//Номера групп, в которых находится студент.
 
 public:
-	Student(string lastName, string firstName, string patronymic);
+	Student(string lastName, string firstName, string patronymic, TypeOfPerformance performance);
 	~Student();
 
-	void escape_from_group(StudentGroup* group);
+	bool escape_from_group(StudentGroup* group);			//Уйти из группы.
 
 	//------------------SET------------------
 	void set_number_of_group(unsigned numberOfGroup);		//Установить номер группы, в которой находится студент.
@@ -32,6 +35,7 @@ public:
 	string get_patronymic();							//Получить отчество.
 	unsigned get_number_of_group(unsigned i);			//Получить номер i - ой группы из numbersOfGroup.
 	unsigned get_count_of_group();						//Получить число групп, в которых находится студент.
+	TypeOfPerformance get_performance();				//Получить успеваемость.
 	//---------------------------------------
 
 	//------------------SHOW------------------
